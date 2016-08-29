41f5dc8322984e1ad8cd633ee676dc607a641b01
Archana R <archana.raghunathan@gmail.com>
2016-08-28 22:27:11 -0700

    first AngularJS commit

---
+++ simpleSpiceSearch/js/controllers/SpicesController.js
@@ -0,0 +1,38 @@
+app.controller('SpicesController',function($scope){
+	$scope.title = "Spices From Various Parts of the World",
+	$scope.keywords = ["spices","cooking","condiments"],
+	$scope.spices = [
+					 {
+						 "name":"Cardamom",
+						 "country":"India",
+					 },
+					 {
+						 "name":"Cloves",
+						 "country":"India"
+					 },
+					 {
+						 "name":"Black Pepper",
+						 "country":"India",
+					 },
+					 {
+						 "name":"Cumin",
+						 "country":"India",
+					 },
+					 {
+						 "name":"Curry Leaves",
+						 "country":"India",
+					 },
+					 {
+						 "name":"Mexican cinnamon",
+						 "country":"Mexico",
+					 },
+					 {
+						 "name":"Turmeric",
+						 "country":"India",
+					 },
+					 {
+						 "name":"Pasilla Negro Chile",
+						 "country":"Mexico",
+					 }
+					];
+	});
